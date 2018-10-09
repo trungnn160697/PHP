9476137fb5ea4b4d2be2b8e1d4a0af33dab59269
Trungnn <trungnn@rabiloo.com>
2018-10-09 12:57:33 +0700

    complete week03

--- Lab03/DateTimeFunction.php
+++ Lab03/DateTimeFunction.php
@@ -20,10 +20,29 @@
 		if(isset($_POST['submit'])){
 			if(isset($_POST['name1'])&&isset($_POST['birthday1'])&&isset($_POST['name2'])&&isset($_POST['birthday2'])){
 				$name1 = $_POST['name1'];
-				$birthday1 = date("d/m/Y", strtotime($_POST['birthday1']));
+				$birthday1 = date("l, F d Y", strtotime($_POST['birthday1']));
 				$name2 = $_POST['name2'];
-				$birthday2 = date("I : d/m/Y", strtotime($_POST['birthday2']));
+				$birthday2 = date("l, F d Y", strtotime($_POST['birthday2']));
+				echo "<br>Tên người thứ nhất: ".$name1." và ngày sinh là ".$birthday1;
+				echo "<br>Tên người thứ hai: ".$name2." và ngày sinh là ".$birthday2;
+				diffirendtDay($birthday1,$birthday2);
+				diffirendAge($birthday1,$birthday2);
 			}
+		}
+
+		function diffirendtDay($date1,$date2){
+			
+			$diff = abs(strtotime($date2) - strtotime($date1));
+	  		$years = floor($diff / (365*60*60*24));
+	 		$months = floor(($diff - $years * 365*60*60*24) / (30*60*60*24));
+	  		$days = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24)/ (60*60*24));
+			echo "<br>Hai ngày cách nhau: ".$years . " năm, " . $months . " tháng, " . $days . " ngày";
+		}
+
+		function diffirendAge($date1,$date2){
+			$diff = abs(strtotime($date2) - strtotime($date1));
+	  		$years = floor($diff / (365*60*60*24));
+			echo "<br>Hai người cách nhau: ".$years." tuổi";
 		}
 	?>
 </body>
